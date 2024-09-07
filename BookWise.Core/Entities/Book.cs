79924539c65428d4ac23c38966797e9108fdd0e6
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-09-07 15:09:49 -0300

    feat: implementing entities classes

---
+++ BookWise.Core/Entities/Book.cs
@@ -0,0 +1,31 @@
+﻿using BookWise.Core.Enums;
+
+namespace BookWise.Core.Entities;
+public class Book : BaseEntity
+{
+    public Book(string title, string description, string iSBN, string author, BookGenreEnum genre, DateTime publishedAt, 
+        int numberOfPages, decimal averageGrade, List<byte> bookCover)
+    {
+        Title = title;
+        Description = description;
+        ISBN = iSBN;
+        Author = author;
+        Genre = genre;
+        PublishedAt = publishedAt;
+        NumberOfPages = numberOfPages;
+        AverageGrade = averageGrade;
+        BookCover = bookCover;
+        Reviews = [];
+    }
+
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public string ISBN { get; private set; }
+    public string Author { get; private set; }
+    public BookGenreEnum Genre { get; private set; }
+    public DateTime PublishedAt { get; private set; }
+    public int NumberOfPages { get; private set; }
+    public decimal AverageGrade { get; private set; }
+    public List<byte> BookCover { get; private set; }
+    public List<Review> Reviews { get; private set; }
+}
